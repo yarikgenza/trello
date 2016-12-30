8aef10e99873e5e13836765e0a961b0070255395
yarikgenza <gsmeter@gmail.com>
2016-12-30 13:05:59 +0200

    realised log out

--- app/components/layout/Header.js
+++ app/components/layout/Header.js
@@ -1,11 +1,37 @@
 import React, {Component} from 'react';
+import {Button} from 'react-bootstrap';
 
 export default class Header extends Component {
+
+  constructor() {
+    super();
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+    window.location.href = '/'
+  }
+
+
   render() {
-    return(
-      <div className="header">
-          <h1>Trello</h1>
-      </div>
-    )
+
+    const token = localStorage.getItem('token');
+
+    if(token) {
+      return(
+        <div className="header">
+            <h1 className="header_positioned">Trello</h1>
+            <div className="logOut">
+              <Button onClick={() => this.logOut()} bsStyle="info">Log out</Button>
+            </div>
+        </div>
+      )
+    } else {
+      return (
+        <div className="header">
+            <h1>Trello</h1>
+        </div>
+      )
+    }
   }
 }
